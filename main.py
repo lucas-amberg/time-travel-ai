19ca9aa2298bc015f8a5bafd41b67c63a812a63a
lucas-amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-02-03 18:55:17 -0800

    Begin working on logic to get API key from user

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 from ai import *
 import customtkinter as tk
+import os
 
 class GUI(tk.CTk):
 
   def __init__(self):
-    super.__init__()
+    super().__init__()
     self.geometry("800x600")
     self.title('Time Travel AI')
+    tk.set_default_color_theme('dark-blue')
   
   def change_title(self, title):
     if title:
       self.title(title)
     
   def get_api_key(self):
+    if os.environ.get('OPENAI_API_KEY'):
+      print('retrieved api key')
+      textbox = tk.CTkLabel(self, text="Retrieving API Key From Environment Variables...", anchor='center', font=("Comic Sans MS", 20))
+      textbox.place(x=400, y=300, anchor='center')
+      return os.environ.get('OPENAI_API_KEY')
+    else:
+      self._clear_frame()
+      textbox = tk.CTkEntry(self, "API Key")
+      textbox.pack()
     
+  def _clear_frame(self):
+    for widget in self.winfo_children():
+      widget.destroy()
 
 
 # Main function
 def main():
   # Create the window
   gui = GUI()
-
+  api_key = gui.get_api_key()
   gui.mainloop()
 
 # Run the program
